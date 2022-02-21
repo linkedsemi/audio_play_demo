b0e99f69168e2e18c22c1120060e0bd54aa5db2b
mingzhou-ls <mingzhou@linkedsemi.com>
2022-02-21 22:58:27 +0800

    Add comments in header file.

--- app/audio_hw.h
+++ app/audio_hw.h
@@ -54,16 +54,107 @@ typedef uint8_t pcm_buf_item_size_t;
 
 #define UNSIGNED_MSB (1 << ((USER_AUDIO_RESOLUTION)*8 - 1))
 
+/**
+ ****************************************************************************************
+ * \brief Get audio output channel 1.
+ * 
+ * \return The GPIO of output IO 1.
+ * 
+ ****************************************************************************************
+ */
 uint8_t get_output_io_1(void);
+/**
+ ****************************************************************************************
+ * \brief Get audio output channel 2.
+ * 
+ * \return The GPIO of output IO 2.
+ * 
+ ****************************************************************************************
+ */
 uint8_t get_output_io_2(void);
+/**
+ ****************************************************************************************
+ * \brief Get address of PCM buffer_0.
+ * 
+ * \return The address of PCM buffer_0.
+ * 
+ ****************************************************************************************
+ */
 uint8_t *get_pcm_buf0(void);
+/**
+ ****************************************************************************************
+ * \brief Get address of PCM buffer_1.
+ * 
+ * \return The address of PCM buffer_1.
+ * 
+ ****************************************************************************************
+ */
 uint8_t *get_pcm_buf1(void);
+/**
+ ****************************************************************************************
+ * \brief Get address of PCM buffer_2.
+ * 
+ * \return The address of PCM buffer_2.
+ * 
+ ****************************************************************************************
+ */
 uint8_t *get_pcm_buf2(void);
+/**
+ ****************************************************************************************
+ * \brief Get address of PCM buffer_3.
+ * 
+ * \return The address of PCM buffer_3.
+ * 
+ ****************************************************************************************
+ */
 uint8_t *get_pcm_buf3(void);
+/**
+ ****************************************************************************************
+ * \brief Function to handle raw PCM data.
+ * 
+ * \param[out]  dst_pcm_buf1         Buffer to contain handled PCM data for channel 1.
+ * \param[out]  dst_pcm_buf2         Buffer to contain handled PCM data for channel 2.
+ * \param[in]   raw_pcm_buf          Buffer to contain input raw PCM data.
+ * 
+ ****************************************************************************************
+ */
 void audio_handle_pcm(void *dst_pcm_buf1, void *dst_pcm_buf2, const pcm_src_item_size_t *raw_pcm_buf);
+/**
+ ****************************************************************************************
+ * \brief Set stop flag for audio play.
+ * 
+ ****************************************************************************************
+ */
 void audio_set_stop_flag(void);
+/**
+ ****************************************************************************************
+ * \brief Audio start play function.
+ * 
+ * \param[in]  audio_base         Buffer address for user audio.
+ * \param[in]  audio_length       Audio data length in units of byte.
+ * 
+ ****************************************************************************************
+ */
 void audio_start(uint32_t audio_base, uint32_t audio_length);
+/**
+ ****************************************************************************************
+ * \brief Stop audio play.
+ * 
+ ****************************************************************************************
+ */
 void audio_stop(void);
+/**
+ ****************************************************************************************
+ * \brief Hardware initialization for audio play.
+ * 
+ ****************************************************************************************
+ */
 void audio_hw_init(void);
+/**
+ ****************************************************************************************
+ * \brief Hardware deinitialization for audio play.
+ * 
+ ****************************************************************************************
+ */
 void audio_hw_deinit(void);
 #endif
